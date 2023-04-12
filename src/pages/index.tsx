344f25faa3aa3668dd3d0984862fa4c761b6976d
Christopher Burns <christopher@everfund.com>
2023-04-12 18:05:09 +0100

    Fixed Path

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Form from "../components/Form";
 import LinkNavigation from "../components/LinkNavigation";
-import Layout from "../components/layout";
+import Layout from "../components/Layout";
 
 export default function Index() {
   return (
